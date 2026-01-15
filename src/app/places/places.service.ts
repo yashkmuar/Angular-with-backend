825567cd534ae54ed608995badf02cec5fb12448
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-15 18:19:15 +0530

    also tackel the duplicacy in the records, if found, suppose if place are already added into the favorite, then in the next time the same place cannot be added

--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -27,7 +27,12 @@ export class PlacesService {
 
   addPlaceToUserPlaces(place: Place) {
     const prevPlaces = this.userPlaces();
-    this.userPlaces.set([...prevPlaces,place]);
+
+    if(!prevPlaces.some((p) => p.id === place.id)){
+        this.userPlaces.set([...prevPlaces,place]);
+    }
+
+    
 
     return  this.httpClient.put('http://localhost:3000/user-places', {
       placeId: place.id,
