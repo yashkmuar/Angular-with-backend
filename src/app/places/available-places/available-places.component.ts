9bd874400c037a00fa9f2bf975507f83debb5569
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-15 13:13:55 +0530

    to handle the error in UI, through backend response code

--- src/app/places/available-places/available-places.component.ts
+++ src/app/places/available-places/available-places.component.ts
@@ -16,6 +16,7 @@ import { map } from 'rxjs';
 export class AvailablePlacesComponent implements OnInit {
   places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
   private destroyRef = inject(DestroyRef);
 
@@ -28,6 +29,9 @@ export class AvailablePlacesComponent implements OnInit {
     .subscribe({
       next: (places) => {
         this.places.set(places);
+      },
+      error: (error) => {
+        this.error.set(error);
       },
       complete: () => { // this function runs when the entire process is complete.
         this.isFetching.set(false);
