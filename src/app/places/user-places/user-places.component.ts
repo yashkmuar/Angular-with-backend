79dc41d19db40a48f0245bbe6363658c7727cf19
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-15 17:53:16 +0530

    loadinguserplaces is also done by signal and a service rather than a separate next operator

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -15,19 +15,16 @@ import { PlacesService } from '../places.service';
   imports: [PlacesContainerComponent, PlacesComponent],
 })
 export class UserPlacesComponent implements OnInit{
-  places = signal<Place[] | undefined>(undefined);
   isFetching = signal(false);
   error = signal('');
   private placesService = inject(PlacesService);
   private destroyRef = inject(DestroyRef);
+  places = this.placesService.loadedUserPlaces;
   
    ngOnInit(){
       this.isFetching.set(true);
       const subscription = this.placesService.loadUserPlaces()
       .subscribe({
-        next: (places) => {
-          this.places.set(places);
-        },
         error: (error: Error) => {
           this.error.set(error.message);
         },
