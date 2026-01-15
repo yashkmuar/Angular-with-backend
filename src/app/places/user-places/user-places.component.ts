59601276841be45366dcf1bddcd7bcd24c9bdca4
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-15 17:00:45 +0530

    initilization of the component is done

--- src/app/places/user-places/user-places.component.ts
+++ src/app/places/user-places/user-places.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { PlacesComponent } from '../places.component';
+import { HttpClient } from '@angular/common/http';
+import { Place } from '../place.model';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-user-places',
@@ -10,5 +13,39 @@ import { PlacesComponent } from '../places.component';
   styleUrl: './user-places.component.css',
   imports: [PlacesContainerComponent, PlacesComponent],
 })
-export class UserPlacesComponent {
+export class UserPlacesComponent implements OnInit{
+  places = signal<Place[] | undefined>(undefined);
+  isFetching = signal(false);
+  error = signal('');
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+  
+   ngOnInit(){
+      this.isFetching.set(true);
+      const subscription = this.httpClient.get<{places: Place[]}>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places), catchError((error) => {
+          console.log(error);
+          return throwError(() => new Error('Something went wrong fetching the available places. Please, try again later!!'));
+        }
+        )
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => { // this function runs when the entire process is complete.
+          this.isFetching.set(false);
+        }
+      });
+  
+      this.destroyRef.onDestroy(() => {
+        subscription.unsubscribe();
+      })
+    }
+  
+
 }
