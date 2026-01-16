95d1bafc35ad634c79805f7b5d5cb467ef99db51
Yash Kumar <yashkumarpal1988@gmail.com>
2026-01-16 09:40:54 +0530

    bug resolved with clone http requests

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ import { AppComponent } from './app/app.component';
 import { HttpHandler, HttpHandlerFn, HttpRequest, provideHttpClient, withInterceptors } from '@angular/common/http';
 
 function loggingInterceptor(request:HttpRequest<unknown>, next:HttpHandlerFn) {
-    const req = request.clone({
-        headers: request.headers.set('X-DEBUG','TESTING')
-    });
+    // const req = request.clone({
+    //     headers: request.headers.set('X-DEBUG','TESTING')
+    // });
     console.log('[Outgoing Request]');
     console.log(request);
-    return next(req);
+    return next(request);
 }
 
 
